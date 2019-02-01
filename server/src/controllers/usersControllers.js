6e271bad9b452147657c53140c4219e87c957a88
Wellerson <wellers0n_@hotmail.com>
2019-02-01 11:46:44 -0200

    img front

--- server/src/controllers/usersControllers.js
+++ server/src/controllers/usersControllers.js
@@ -12,3 +12,20 @@ export async function list_all_users(ctx) {
   }
 
 }
+
+export async function add_user(ctx){
+  try{
+    let user = await ctx.request.header;
+    console.log(user)
+    ctx.body = user;
+    Users({
+      nome: user.nome,
+      email: user.email,
+      senha: user.senha
+    }).save()
+
+  }catch(err){
+    ctx.status = err.status || 500;
+    ctx.body = err.message;
+  }
+}
--- server/src/routes/usersRouter.js
+++ server/src/routes/usersRouter.js
@@ -1,9 +1,11 @@
 
-import { list_all_users } from './../controllers/usersControllers.js'
+import { list_all_users, add_user} from './../controllers/usersControllers.js'
 
 export default function(router){
 
-  router.get('/users', list_all_users)
+  router.get('/users', list_all_users);
+
+  router.post('/user', add_user);
 }
 
 
